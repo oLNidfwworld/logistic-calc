bd3f1d162c127d3e3bd179d065d619cfc671902a
oLNidfwworld <kuvetov@bk.ru>
2024-07-16 12:23:51 +0300

    fix: change build

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'vueBundle.js',
+        assetFileNames: 'vueStyles.css',
+        chunkFileNames: 'vueChunk.js',
+        manualChunks: undefined
+      }
     }
   }
 })
